31c8f815f6e9c6a9a5e712e2ac3ee4014aa45f53
Serhii Voichyk <svoychik@gmail.com>
2023-08-03 21:19:58 +0200

    Removed not needed

--- build.cmd
+++
@@ -1,3 +0,0 @@
-dotnet restore
-dotnet lambda package ./src/MoviesApi.csproj --configuration release --framework net6.0 --output-package bin/release/net6.0/hello.zip
-
--- build.sh
+++ build.sh
@@ -6,7 +6,5 @@ then
   apt -qq update
   apt -qq -y install zip
 fi
-echo Hi
-dotnet restore
 
 dotnet lambda package --project-location src/MoviesApi --configuration release --framework net6.0 --output-package bin/release/net6.0/hello.zip

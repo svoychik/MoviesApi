487007332c8ec65684cecc28784ec9e304c36b4f
Serhii Voichyk <svoychik@gmail.com>
2023-08-29 11:26:43 +0200

    Reorginized code

--- examples/3-movies-api-with-asp-net-core-lambda-url/src/MoviesApi.AspNetCoreServer/Program.cs
+++ examples/3-movies-api-with-asp-net-core-lambda-url/src/MoviesApi.AspNetCoreServer/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,7 +12,7 @@ builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -26,12 +26,23 @@ app.MapControllers();
 app.MapGet("/", () => "Hello from AWS Lambda!");
 app.MapGet("/movies", () => new List<Movie>()
 {
-    new Movie() {Name = "Movie1"},
-    new Movie() {Name = "Movie2"}
+    new Movie() { Id = 1, Name = "Movie1" },
+    new Movie() { Id = 2, Name = "Movie2" }
 });
+app.MapGet(
+    "/movies/{id}", 
+    (int id) => new Movie() { Name = "Movie1", Id = id }
+);
+
+app.MapPost(
+    "/movies",
+    (Movie movie) => movie
+);
+
 app.Run();
 
 public class Movie
 {
+    public int Id { get; set; }
     public string Name { get; set; }
 }
